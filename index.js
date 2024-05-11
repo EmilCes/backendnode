986b122a3ede266b99426f6c3a77f369e564eb6a
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-05-10 19:11:31 -0600

    First Commit

--- index.js
+++ index.js
@@ -29,7 +29,16 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 // Rutas
 app.use("/api/categorias", require('./routes/categorias.routes'));
 app.use("/api/peliculas", require('./routes/peliculas.routes'));
+app.use("/api/usuarios", require('./routes/usuarios.routes'));
+app.use("/api/roles", require('./routes/roles.routes'));
+app.use("/api/auth", require('./routes/auth.routes'));
 app.get('*', (req, res) => { res.status(404).send() });
+
+// Middleware para el manejo de errores (Debe ser el último middleware a utilizar)
+const errorlogger = require('./middlewares/errorlogger.middleware');
+const errorhandler = require('./middlewares/errorhandler.middleware');
+app.use(errorlogger, errorhandler);
+
 
 // Inicia el servidor web en el puerto SERVER_PORT
 app.listen(process.env.SERVER_PORT, () => {
